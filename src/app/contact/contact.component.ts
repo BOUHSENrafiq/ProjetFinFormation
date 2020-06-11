a286059157e2a8e5ad2ee1ecbd546640f12412a5
BOUHSENrafiq <58845334+BOUHSENrafiq@users.noreply.github.com>
2020-06-11 01:35:33 +0100

    ComponentContact done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import Contact from '../models/contact';
 import {ContactService} from '../services/contact.service';
